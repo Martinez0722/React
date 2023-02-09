7fe4deca80bf89b874f352def6baf96121b94a5f
Rafael Felipe <102329554+Martinez0722@users.noreply.github.com>
2023-02-09 19:03:08 -0300

    .

--- Fudamentos/inicial/src/App.js
+++ Fudamentos/inicial/src/App.js
@@ -4,6 +4,7 @@ import TemplateExpressions from './components/TemplateExpressions';
 import MyComponent from './components/MyComponent';
 import Events from './components/Events';
 import Challenge from './components/Challenge';
+import PorDoSol from './assets/por-do-sol.jpg'
 
 // Styles / css
 import './App.css';
@@ -11,12 +12,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Fundamentos React</h1>
-      {/* <FirstComponent />
-      <TemplateExpressions />
-      <MyComponent />
-      <Events /> */}
-      <Challenge />
+      <h1>Avançando em React</h1>
+      {/* Imagem em public */}
+      <div>
+        <img src='/cyber.jpg' alt='tokyo'/>
+      </div>
+      {/* Imagem em assets */}
+      <div>
+        <img src={PorDoSol} alt='por-do-sol'/>
+      </div>
     </div>
   );
 }
